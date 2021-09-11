004c259def85aea4c90055dfa753e5e4096f7a11
mddd <413153189@qq.com>
2021-09-11 13:39:13 +0800

    细节调整

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,21 +1,20 @@
-import TaokeApi from 'dd_server_api/apis/taoke';
-import DdTaokeSdk from 'dd_server_api/apis';
+import DdServerApiByWeb from 'dd_server_api_web/apis';
 
 const host = 'https://itbug.shop';
-/**
- * 淘客api接口
- */
-export const taokeApi = (): TaokeApi => {
-  const api = TaokeApi.getInstance();
-  api.host = host;
-  return api;
-};
+// /**
+//  * 淘客api接口
+//  */
+// export const taokeApi = (): TaokeApi => {
+//   const api = TaokeApi.getInstance();
+//   api.host = host;
+//   return api;
+// };
 
 /**
  * 博客api接口
  */
-export const blogApi = (): DdTaokeSdk => {
-  const api = DdTaokeSdk.getInstance();
+export const blogApi = (): DdServerApiByWeb => {
+  const api = DdServerApiByWeb.getInstance();
   api.host = host;
   return api;
 };
