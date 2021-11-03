d3f60c5fab255a07a925339992da4cf302850b0a
eee <hlxsmail@gmail.com>
2021-11-03 16:37:17 +0800

    细节调整

--- .umirc.ts
+++ .umirc.ts
@@ -8,4 +8,12 @@ export default defineConfig({
   routes: routers,
   fastRefresh: {},
   mfsu: {},
+  externals: {
+    react: 'window.React',
+    'react-dom': 'window.ReactDOM',
+  },
+  scripts: [
+    'https://gw.alipayobjects.com/os/lib/react/17.0.2/umd/react.production.min.js',
+    'https://gw.alipayobjects.com/os/lib/react-dom/17.0.2/umd/react-dom.production.min.js',
+  ],
 });
--- config/config.ts
+++ config/config.ts
@@ -4,4 +4,21 @@ import routes from './routers';
 export default defineConfig({
   routes,
   mfsu: {},
+  externals: {
+    react: 'window.React',
+    'react-dom': 'window.ReactDOM',
+  },
+  scripts: [
+    'https://gw.alipayobjects.com/os/lib/react/17.0.2/umd/react.production.min.js',
+    'https://gw.alipayobjects.com/os/lib/react-dom/17.0.2/umd/react-dom.production.min.js',
+  ],
+  cacheGroups: {
+    vendors: {
+      name: 'vendors',
+      chunks: 'all',
+      test: /[\\/]node_modules[\\/]/,
+      priority: -12,
+    },
+  },
+  chunks: ['vendors', 'umi'],
 });
