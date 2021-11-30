5ba108539182f3e9fa84813db5a36b006f6ed131
mdddj <413153189@qq.com>
2021-11-30 16:15:50 +0800

    新增友链的相关功能

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,15 +1,54 @@
 import BaseLayout from '@/components/BaseLayout';
+import { blogApi } from '@/util/request';
+import Center from '@/widgets/Center';
 import MarkdownText from '@/widgets/MarkdownText';
 import SizedBox from '@/widgets/SizedBox';
-import { Typography } from '@mui/material';
-import { Button, Form, Input } from 'antd';
+import { Popover, Typography } from '@mui/material';
+import { Button, Form, Input, message } from 'antd';
+import {
+  Result,
+  successResultHandle,
+} from 'dd_server_api_web/src/utils/ResultUtil';
+import { ReactHTMLElement, useState } from 'react';
 
 /// 友链页面
 const Friends: React.FC = () => {
+  const [ell, setell] = useState<null | HTMLElement>(null);
+  const isopen = Boolean(ell);
+  const id = isopen ? 'fr-pp' : undefined;
+
   return (
-    <BaseLayout appbarCurrent="友链" rightContainer={<FriendForm />}>
-      <Button>申请友链</Button>
+    <BaseLayout appbarCurrent="友链">
+      <Center>
+        <Button
+          onClick={(e) => {
+            setell(e.currentTarget);
+          }}
+        >
+          申请友链
+        </Button>
+      </Center>
+
       <MarkdownText findKey="friend-page-desc" />
+
+      <Popover
+        open={isopen}
+        onClose={() => {
+          setell(null);
+        }}
+        anchorEl={ell}
+        id={id}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <FriendForm
+          success={() => {
+            setell(null);
+          }}
+        />
+      </Popover>
     </BaseLayout>
   );
 };
@@ -17,26 +56,41 @@ const Friends: React.FC = () => {
 export default Friends;
 
 /// 提交友链的表单
-const FriendForm: React.FC = () => {
+const FriendForm: React.FC<{ success: () => void }> = (props) => {
+  const submit = async (values: any) => {
+    const result = await blogApi().saveFriendsLink(values);
+    successResultHandle(
+      result,
+      (d) => {
+        message.success(result.message);
+        props.success();
+      },
+      message.error,
+    );
+  };
+
   return (
-    <div className="base-card">
-      <Typography>申请友链</Typography>
+    <div className="base-card" style={{ width: 600 }}>
+      <Typography variant="h2">申请友链</Typography>
       <SizedBox height={12} />
-      <Form layout="vertical">
-        <Form.Item label="名称" name="name">
+      <Form layout="vertical" onFinish={submit}>
+        <Form.Item label="名称" name="name" required>
           <Input />
         </Form.Item>
-        <Form.Item label="链接" name="url">
+        <Form.Item label="链接" name="url" required>
+          <Input type="url" />
+        </Form.Item>
+        <Form.Item label="一句话介绍" name="intro" required>
           <Input />
         </Form.Item>
-        <Form.Item label="一句话介绍" name="intro">
-          <Input />
+        <Form.Item label="头像" name="logo" required>
+          <Input type="url" />
         </Form.Item>
-        <Form.Item label="头像" name="logo">
-          <Input />
+        <Form.Item label="邮箱" name="email">
+          <Input type="email" />
         </Form.Item>
         <Form.Item>
-          <Button>提交审核</Button>
+          <Button htmlType="submit">提交审核</Button>
         </Form.Item>
       </Form>
     </div>
---
+++ src/widgets/Center.tsx
@@ -0,0 +1,7 @@
+const Center: React.FC = (prosp) => {
+  return (
+    <div style={{ textAlign: 'center', margin: 12 }}>{prosp.children}</div>
+  );
+};
+
+export default Center;
